a90163ff86f295baec84d65813c4e7f14ebcdd10
Sullisters <heatonpschreiber@gmail.com>
2022-10-27 22:54:04 -0400

    finalized code

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const express = require('express');
+const path = require('path');
 //Method for generating unique ids
 const uuid = require('./helper/uuid');
-const notes = require('./db/db.json')
-const path = require('path')
+
+const notes = require('./db/db.json');
 
 const PORT = 3001;
 
@@ -14,20 +15,21 @@ app.use(express.urlencoded({extended: true }));
 
 app.use(express.static('public'));
 
-//GET request for homepage
-app.get('*', (req,res)=>
-    res.sendFile(path.join(__dirname, '/assets/index.html'))
-    );
-
 //GET request for the notes page
 app.get('/notes', (req,res)=>
-    res.sendFile(path.join(__dirname, '/assets/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+
 
 //GET request for /api/notes
 app.get('/api/notes', (req,res)=> {
     res.status(200).json(notes);
 });
+
+//GET request for homepage
+app.get('*', (req,res)=>
+    res.sendFile(path.join(__dirname, 'index.html'))
+    );
 
 //POST request to add to the notes.json
 app.post('/api/notes', (req,res)=> {
@@ -45,6 +47,31 @@ app.post('/api/notes', (req,res)=> {
             text,
             note_id: uuid(),
         };
+
+        //Obtain existing notes
+        fs.readFile('./db/db.json', 'utf8', (err, data)=>{
+            if (err){
+                console.error(err);
+            } else {
+                //Convert string into JSON object
+                const parsedNotes = JSON.parse(data);
+
+                //Add new note
+                parsedNotes.push(newNote);
+        
+                //Write updated notes back to the file
+                fs.writeFile(
+                    './db/db.json', 
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr)=>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes')
+                );
+                
+            }
+        })
+
 
         const response = {
             status: 'success',
